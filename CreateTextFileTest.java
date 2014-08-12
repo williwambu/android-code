188196dfc23eb59cfb9b1e2920270e589efbea54
william <willi.wambu@gmail.com>
2014-08-12 14:06:31 +0300

    More classes

---
+++ CreateTextFileTest.java
@@ -0,0 +1,11 @@
+// Testing the CreateTextFile class.
+public class CreateTextFileTest
+{	
+public static void main( String[] args )
+{
+CreateTextFile application = new CreateTextFile();
+application.openFile();
+application.addRecords();
+application.closeFile();
+} // end main
+} // end class CreateTextFileTest
